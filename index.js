210645ebb7bef32d12d4bcbab0c66cdb5e646967
SwartzJonah <swartzjonah@gmail.com>
2023-03-23 16:24:23 -0400

    added prepend and toString function

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function Node(value = null, next = null) {
 }
 
 function LinkedList() {
-    let head = null;
+    this.head = null;
 
-    function append(value){
+    //Add value to end of list
+    function append(value) {
         let node = Node(value);
-        console.log(node);
         if (this.head === null) {
             this.head = node;
         } else {
@@ -17,12 +17,43 @@ function LinkedList() {
             }
             tail.next = node;
         }
-        console.log(this)
+
     }
 
-    return {head, append}
+    //add value to front of list
+    function prepend(value) {
+        let node = Node(value);
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            let temp = this.head;
+            this.head = node;
+            this.head.next = temp;
+        }
+
+    }
+
+    //prints list as a string
+    function toString() {
+        let stringList = ""
+        if (this.head === null) {
+            stringList = "( empty ) "
+        } else {
+            let temp = this.head;
+            while (temp.next != null) {
+                stringList += "( " + temp.value + " ) -> ";
+                temp = temp.next;
+            }
+            stringList += "( " + temp.value + " )";
+        }
+        console.log(stringList);
+    }
+
+
+    return { head, append, prepend, toString }
 }
 
 const test = LinkedList();
-console.log(test.append(2))
-console.log(test.append(4))
+test.append(2)
+test.prepend(6)
+test.toString();
